33b49fac4e04c5f28728289720873bdfc1c36926
Devlin <devlincodes@gmail.com>
2024-11-19 22:16:29 -0800

    refactor collide and explode code

--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -9,20 +9,12 @@ const SPEED := 8
 
 
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
 	area_entered.connect(_on_area_entered)
 	lifetime.timeout.connect(_on_lifetime_timeout)
 
 
-func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("enemies"):
-		explode()
-		body.hit.emit()
-
-
 func _on_area_entered(area: Area2D) -> void:
-	area.queue_free()
-	queue_free()
+	explode()
 
 
 func _physics_process(delta: float) -> void:
@@ -37,3 +29,4 @@ func explode() -> void:
 	const EXPLOSION := preload("res://effects/explosion.tscn")
 	var new_explosion := EXPLOSION.instantiate()
 	new_explosion.explode(self)
+	queue_free()
