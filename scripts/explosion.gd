fe6d47470b2003636a867aaa455517b8e814c56c
Devlin <devlincodes@gmail.com>
2024-11-19 22:15:58 -0800

    refactor `_process` into signal timeout

--- scripts/explosion.gd
+++ scripts/explosion.gd
@@ -1,14 +1,9 @@
 extends GPUParticles2D
 
 
-func _process(delta: float) -> void:
-	if not emitting:
-		queue_free()
-
-
 func explode(node: Node) -> void:
 	var new_explosion := duplicate()
 	node.add_sibling(new_explosion)
 	new_explosion.global_position = node.global_position
 	new_explosion.emitting = true
-	node.queue_free()
+	new_explosion.finished.connect(queue_free)
